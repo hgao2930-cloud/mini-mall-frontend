d1323bbe96e3d258291f2985778d07c36af7271a
hgao2930-cloud <hgao2930@gmail.com>
2026-09-13 22:32:38 +0800

    chore: 移除 App.vue 中未使用的 debounce 函数

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue'
-function debounce<T extends unknown[]>(fn:(...args:T)=>void,delay:number){
-  let timer:number|null = null
-  return function debounced(...args:T){
-    if(timer!==null)clearTimeout(timer)
-    timer = setTimeout(() => {
-      fn(...args)
-      timer = null
-    }, delay);
-  }
-}
 </script>
 
 <template>
